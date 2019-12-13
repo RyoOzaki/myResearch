214800cc44563edb41c95cde1a7f82bfdf8801bc
RyoOzaki <ryo.ozaki@em.ci.ritsumei.ac.jp>
2019-12-13 20:38:18 +0900

    add feature_extraction for starganvc

--- Experiment_1/feature_extracting.sh
+++ Experiment_1/feature_extracting.sh
@@ -12,6 +12,14 @@ python src/Extractor/collect2npz.py --source_dir dataset/ --output_dir feature/ 
 mv feature/mcep.npz feature/mcep_all_speaker.npz
 
 #==============================================
+# extract mcep by multispeaker_AIOI data
+# output
+#   feature/mcep_all_speaker_5msec.npz (renamed from mcep.npz)
+python src/Extractor/extract_world.py --source_dir dataset/ --feature_type mcep --frame_period 5E-3 --nfilt 36
+python src/Extractor/collect2npz.py --source_dir dataset/ --output_dir feature/ --collect_extensions mcep --with_speaker_id --speaker_dir_layer 1
+mv feature/mcep.npz feature/mcep_all_speaker_5msec.npz
+
+#==============================================
 # pickup each speaker
 # output
 #   feature/mcep_speaker_H.npz (pickup by mcep_all_speaker.npz)
@@ -22,3 +30,7 @@ python src/Extractor/pickup.py --source_file feature/mcep_all_speaker.npz --outp
 python src/Extractor/pickup.py --source_file feature/mcep_all_speaker.npz --output_file feature/mcep_speaker_K.npz --keyword speaker_K
 python src/Extractor/pickup.py --source_file feature/mcep_all_speaker.npz --output_file feature/mcep_speaker_M.npz --keyword speaker_M
 python src/Extractor/pickup.py --source_file feature/mcep_all_speaker.npz --output_file feature/mcep_speaker_N.npz --keyword speaker_N
+
+#==============================================
+# clean dataset dir
+python src/Extractor/clean.py --source_dir dataset/ --remove_extensions mcep wrd phn
