5ab6f309871ab4c933b2e0ca0b7c46c0157f2725
RyoOzaki <ryo.ozaki@em.ci.ritsumei.ac.jp>
2019-12-13 20:46:13 +0900

    rename mcep_all_speaker.npz to mcep_all_speaker_20msec.npz

--- Experiment_1/compress.sh
+++ Experiment_1/compress.sh
@@ -3,12 +3,12 @@
 #==============================================
 PCA_COMPONENTS="5"
 # compress mcep using PCA
-#   feature/pca_all_speaker.npz (compress mcep_all_speaker.npz)
+#   feature/pca_all_speaker.npz (compress mcep_all_speaker_20msec.npz)
 #   feature/pca_speaker_H.npz (compress mcep_speaker_H.npz)
 #   feature/pca_speaker_K.npz (compress mcep_speaker_K.npz)
 #   feature/pca_speaker_M.npz (compress mcep_speaker_M.npz)
 #   feature/pca_speaker_N.npz (compress mcep_speaker_N.npz)
-python src/Compress/PCA_compress.py --source_file feature/mcep_all_speaker.npz --output_file feature/pca_all_speaker.npz --n_components ${PCA_COMPONENTS}
+python src/Compress/PCA_compress.py --source_file feature/mcep_all_speaker_20msec.npz --output_file feature/pca_all_speaker.npz --n_components ${PCA_COMPONENTS}
 python src/Compress/PCA_compress.py --source_file feature/mcep_speaker_H.npz --output_file feature/pca_speaker_H.npz --n_components ${PCA_COMPONENTS}
 python src/Compress/PCA_compress.py --source_file feature/mcep_speaker_K.npz --output_file feature/pca_speaker_K.npz --n_components ${PCA_COMPONENTS}
 python src/Compress/PCA_compress.py --source_file feature/mcep_speaker_M.npz --output_file feature/pca_speaker_M.npz --n_components ${PCA_COMPONENTS}
@@ -19,16 +19,16 @@ DSAE_STRUCTURE="36 18 9 5"
 DSAE_PBHL_STRUCTURE="4 3"
 
 # compress mcep using DSAE and DSAE-PBHL
-#   feature/dsae_all_speaker.npz (compress mcep_all_speaker.npz)
-#   feature/dsae_pbhl_all_speaker.npz (compress mcep_all_speaker.npz and speaker.npz)
-#   feature/dsae_pbhl_v2_all_speaker.npz (compress mcep_all_speaker.npz and speaker.npz)
+#   feature/dsae_all_speaker.npz (compress mcep_all_speaker_20msec.npz)
+#   feature/dsae_pbhl_all_speaker.npz (compress mcep_all_speaker_20msec.npz and speaker.npz)
+#   feature/dsae_pbhl_v2_all_speaker.npz (compress mcep_all_speaker_20msec.npz and speaker.npz)
 #   feature/dsae_speaker_H.npz (compress mcep_speaker_H.npz)
 #   feature/dsae_speaker_K.npz (compress mcep_speaker_K.npz)
 #   feature/dsae_speaker_M.npz (compress mcep_speaker_M.npz)
 #   feature/dsae_speaker_N.npz (compress mcep_speaker_N.npz)
-python src/DSAE-PBHL/DSAE_train.py --train_data feature/mcep_all_speaker.npz --output_file feature/dsae_all_speaker.npz --structure ${DSAE_STRUCTURE}
-python src/DSAE-PBHL/DSAE_PBHL_train.py --train_data feature/mcep_all_speaker.npz --speaker_id feature/speaker.npz  --output_file feature/dsae_pbhl_all_speaker.npz --structure ${DSAE_STRUCTURE} --pb_structure ${DSAE_PBHL_STRUCTURE}
-python src/DSAE-PBHL/DSAE_PBHL_v2_train.py --train_data feature/mcep_all_speaker.npz --speaker_id feature/speaker.npz  --output_file feature/dsae_pbhl_v2_all_speaker.npz --structure ${DSAE_STRUCTURE} --pb_structure ${DSAE_PBHL_STRUCTURE}
+python src/DSAE-PBHL/DSAE_train.py --train_data feature/mcep_all_speaker_20msec.npz --output_file feature/dsae_all_speaker.npz --structure ${DSAE_STRUCTURE}
+python src/DSAE-PBHL/DSAE_PBHL_train.py --train_data feature/mcep_all_speaker_20msec.npz --speaker_id feature/speaker.npz  --output_file feature/dsae_pbhl_all_speaker.npz --structure ${DSAE_STRUCTURE} --pb_structure ${DSAE_PBHL_STRUCTURE}
+python src/DSAE-PBHL/DSAE_PBHL_v2_train.py --train_data feature/mcep_all_speaker_20msec.npz --speaker_id feature/speaker.npz  --output_file feature/dsae_pbhl_v2_all_speaker.npz --structure ${DSAE_STRUCTURE} --pb_structure ${DSAE_PBHL_STRUCTURE}
 python src/DSAE-PBHL/DSAE_train.py --train_data feature/mcep_speaker_H.npz --output_file feature/dsae_speaker_H.npz --structure ${DSAE_STRUCTURE}
 python src/DSAE-PBHL/DSAE_train.py --train_data feature/mcep_speaker_K.npz --output_file feature/dsae_speaker_K.npz --structure ${DSAE_STRUCTURE}
 python src/DSAE-PBHL/DSAE_train.py --train_data feature/mcep_speaker_M.npz --output_file feature/dsae_speaker_M.npz --structure ${DSAE_STRUCTURE}
--- Experiment_1/run_NPB_DAA.sh
+++ Experiment_1/run_NPB_DAA.sh
@@ -17,7 +17,7 @@ sh src/NPB-DAA/watchdog_runner.sh -t feature/dsae_speaker_M.npz -p feature/phn.n
 sh src/NPB-DAA/watchdog_runner.sh -t feature/dsae_speaker_N.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/dsae_speaker_N
 
 python src/NPB-DAA/unroll_default_config.py --default_config default_configs/mcep_defaults.config
-sh src/NPB-DAA/watchdog_runner.sh -t feature/mcep_all_speaker.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/mcep_all_speaker
+sh src/NPB-DAA/watchdog_runner.sh -t feature/mcep_all_speaker_20msec.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/mcep_all_speaker
 sh src/NPB-DAA/watchdog_runner.sh -t feature/mcep_speaker_H.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/mcep_speaker_H
 sh src/NPB-DAA/watchdog_runner.sh -t feature/mcep_speaker_K.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/mcep_speaker_K
 sh src/NPB-DAA/watchdog_runner.sh -t feature/mcep_speaker_M.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/mcep_speaker_M
