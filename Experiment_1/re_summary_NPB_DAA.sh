6081b53b8722b73cc9b945446e45777963e712b4
RyoOzaki <ryo.ozaki@em.ci.ritsumei.ac.jp>
2019-12-17 23:13:58 +0900

    add re-summary script

---
+++ Experiment_1/re_summary_NPB_DAA.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+dirs="segmentation_result/*"
+
+for dirpath in ${dirs};
+do
+  # echo ${dirpath}
+  for i in `seq 1 20`
+  do
+    i_str=$( printf '%02d' $i )
+    echo "${dirpath}/${i_str}"
+    python src/NPB-DAA/summary.py --phn_label ${dirpath}/phn.npz --wrd_label ${dirpath}/wrd.npz --model ${dirpath}/hypparams/model.config --results_dir ${dirpath}/${i_str}/results --figure_dir ${dirpath}/${i_str}/figures --summary_dir ${dirpath}/${i_str}/summary_files
+  done
+  python src/NPB-DAA/summary_summary.py --result_dir ${dirpath} --figure_dir ${dirpath}/figures --summary_dir ${dirpath}/summary_files
+done
