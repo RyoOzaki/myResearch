b38bff60cecab1f6f18d11db63155b4f49eaace0
RyoOzaki <ryo.ozaki@em.ci.ritsumei.ac.jp>
2019-12-19 03:28:48 +0900

    remove commentout

--- Experiment_1/run_NPB_DAA.sh
+++ Experiment_1/run_NPB_DAA.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-# python src/NPB-DAA/unroll_default_config.py --default_config default_configs/compressed_defaults.config
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/pca_all_speaker.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/pca_all_speaker
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/pca_speaker_H.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/pca_speaker_H
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/pca_speaker_K.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/pca_speaker_K
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/pca_speaker_M.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/pca_speaker_M
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/pca_speaker_N.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/pca_speaker_N
-#
-# python src/NPB-DAA/unroll_default_config.py --default_config default_configs/compressed_defaults.config
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/dsae_all_speaker.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/dsae_all_speaker
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/dsae_pbhl_all_speaker.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/dsae_pbhl_all_speaker
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/dsae_pbhl_v2_all_speaker.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/dsae_pbhl_v2_all_speaker
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/dsae_speaker_H.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/dsae_speaker_H
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/dsae_speaker_K.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/dsae_speaker_K
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/dsae_speaker_M.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/dsae_speaker_M
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/dsae_speaker_N.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/dsae_speaker_N
-#
-# python src/NPB-DAA/unroll_default_config.py --default_config default_configs/compressed_defaults.config
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/sgvc_all_speaker.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/sgvc_all_speaker
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/sgvc_new_all_speaker.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/sgvc_new_all_speaker
-#
-# python src/NPB-DAA/unroll_default_config.py --default_config default_configs/mcep_defaults.config
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/mcep_all_speaker_20msec.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/mcep_all_speaker
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/mcep_speaker_H.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/mcep_speaker_H
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/mcep_speaker_K.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/mcep_speaker_K
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/mcep_speaker_M.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/mcep_speaker_M
-# sh src/NPB-DAA/watchdog_runner.sh -t feature/mcep_speaker_N.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/mcep_speaker_N
+python src/NPB-DAA/unroll_default_config.py --default_config default_configs/compressed_defaults.config
+sh src/NPB-DAA/watchdog_runner.sh -t feature/pca_all_speaker.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/pca_all_speaker
+sh src/NPB-DAA/watchdog_runner.sh -t feature/pca_speaker_H.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/pca_speaker_H
+sh src/NPB-DAA/watchdog_runner.sh -t feature/pca_speaker_K.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/pca_speaker_K
+sh src/NPB-DAA/watchdog_runner.sh -t feature/pca_speaker_M.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/pca_speaker_M
+sh src/NPB-DAA/watchdog_runner.sh -t feature/pca_speaker_N.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/pca_speaker_N
+
+python src/NPB-DAA/unroll_default_config.py --default_config default_configs/compressed_defaults.config
+sh src/NPB-DAA/watchdog_runner.sh -t feature/dsae_all_speaker.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/dsae_all_speaker
+sh src/NPB-DAA/watchdog_runner.sh -t feature/dsae_pbhl_all_speaker.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/dsae_pbhl_all_speaker
+sh src/NPB-DAA/watchdog_runner.sh -t feature/dsae_pbhl_v2_all_speaker.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/dsae_pbhl_v2_all_speaker
+sh src/NPB-DAA/watchdog_runner.sh -t feature/dsae_speaker_H.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/dsae_speaker_H
+sh src/NPB-DAA/watchdog_runner.sh -t feature/dsae_speaker_K.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/dsae_speaker_K
+sh src/NPB-DAA/watchdog_runner.sh -t feature/dsae_speaker_M.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/dsae_speaker_M
+sh src/NPB-DAA/watchdog_runner.sh -t feature/dsae_speaker_N.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/dsae_speaker_N
+
+python src/NPB-DAA/unroll_default_config.py --default_config default_configs/compressed_defaults.config
+sh src/NPB-DAA/watchdog_runner.sh -t feature/sgvc_all_speaker.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/sgvc_all_speaker
+sh src/NPB-DAA/watchdog_runner.sh -t feature/sgvc_new_all_speaker.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/sgvc_new_all_speaker
+
+python src/NPB-DAA/unroll_default_config.py --default_config default_configs/mcep_defaults.config
+sh src/NPB-DAA/watchdog_runner.sh -t feature/mcep_all_speaker_20msec.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/mcep_all_speaker
+sh src/NPB-DAA/watchdog_runner.sh -t feature/mcep_speaker_H.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/mcep_speaker_H
+sh src/NPB-DAA/watchdog_runner.sh -t feature/mcep_speaker_K.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/mcep_speaker_K
+sh src/NPB-DAA/watchdog_runner.sh -t feature/mcep_speaker_M.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/mcep_speaker_M
+sh src/NPB-DAA/watchdog_runner.sh -t feature/mcep_speaker_N.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/mcep_speaker_N
 
 python src/NPB-DAA/unroll_default_config.py --default_config default_configs/mcep_defaults.config
 sh src/NPB-DAA/watchdog_runner.sh -t feature/norm_all_speaker.npz -p feature/phn.npz -w feature/wrd.npz -l segmentation_result/norm_all_speaker
